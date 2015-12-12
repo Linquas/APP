55650b76d9fb23dc9057e60415bd3e410dd95702
CHTim <ice37ya@yahoo.com.tw>
2015-12-12 16:45:00 +0800

    add country to control the result (temperature not yet)

--- app/src/main/java/com/example/linquas/myapplication/Main3Activity.java
+++ app/src/main/java/com/example/linquas/myapplication/Main3Activity.java
@@ -8,7 +8,9 @@ import android.widget.ImageView;
 public class Main3Activity extends AppCompatActivity {
 
     private static final String TAG = "Main3Activity";
-    int[] image = {R.drawable.img1,R.drawable.img2,R.drawable.img3,R.drawable.img4};
+    int[] image = {R.drawable.img0,R.drawable.img1,R.drawable.img2,R.drawable.img3,R.drawable.img4,
+            R.drawable.img5,R.drawable.img6,R.drawable.img7,R.drawable.img8,R.drawable.img9,
+            R.drawable.img10,R.drawable.img11,R.drawable.img12};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
